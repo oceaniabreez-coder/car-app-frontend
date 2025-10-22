c7b1c6b4fbaac735b616143d3307f16e118804dc
Ruwanga Karunarathna <ruwanga@Ruwangas-MacBook-Air.local>
2025-10-22 12:06:44 +1100

    add loading

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,19 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient,withInterceptorsFromDi } from '@angular/common/http';
+import { NgxUiLoaderModule, NgxUiLoaderHttpModule } from 'ngx-ui-loader';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideHttpClient(),
+    provideHttpClient(withInterceptorsFromDi()),
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes)
+    provideRouter(routes),
+    importProvidersFrom(
+      NgxUiLoaderModule,                           
+      NgxUiLoaderHttpModule.forRoot({ showForeground: true })
+    )
   ]
 };
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CarHomeComponent } from "./components/car-home-component/car-home-component";
+import { NgxUiLoaderModule } from 'ngx-ui-loader';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CarHomeComponent],
+  imports: [RouterOutlet, CarHomeComponent,NgxUiLoaderModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
